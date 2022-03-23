2b41328ef5e284d64580bec30cc7554e6c041153
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-03-23 17:26:26 -0500

    added notes

--- Queue/Program.cs
+++ Queue/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+/*
+  What is Queue<T> Class?
+
+Definition
+Namespace:
+System.Collections.Generic
+
+Assembly:
+System.Collections.dll
+
+Represents a first-in, first-out collection of objects.
+
+
+
+
+ */
